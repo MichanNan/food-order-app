0eb483411b3a497fa800c39dd4b5bb753344d131
Nan <caonanla@gmail.com>
2023-07-18 22:28:54 +0200

    add context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartPorvider from "./store/CartProvider";
 
-import { Fragment } from "react";
+import { useState } from "react";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHanlder = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <Fragment>
-      <Header />
-    </Fragment>
+    <CartPorvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHanlder} />
+      <main>
+        <Meals />
+      </main>
+    </CartPorvider>
   );
 }
 
