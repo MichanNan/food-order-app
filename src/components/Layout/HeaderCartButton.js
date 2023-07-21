2f5870d46791d44864695d2a92f5cd63eed549b1
Nan <caonanla@gmail.com>
2023-07-21 15:08:35 +0200

    connect firebase

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
@@ -6,11 +6,34 @@ import CartContext from "../../store/cart-context";
 export default function HeaderCartButton(props) {
   const cartCtx = useContext(CartContext);
 
-  const numberIfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+  const { items } = cartCtx;
+
+  const numberIfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
